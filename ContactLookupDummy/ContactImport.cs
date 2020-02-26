b382eb786247ad881ce7ef6e4e8d274008d0b6a7
Penny <penyamy@gmail.com>
2020-02-26 19:13:34 +0200

    Added contact importing

---
+++ ContactLookupDummy/ContactImport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactLookupDummy
+{
+    class ContactImport : JsonImport<ICollection<Contact>>
+    {
+        public ICollection<Contact> ImportContacts(string fileName = "Contacts")
+        {
+            return JsonSave(fileName);
+        }
+    }
+}
